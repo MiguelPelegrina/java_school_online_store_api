4c543d7c85e8a9751ff737ab95afe21de8f04490
migup <migupele@gmail.com>
2023-10-04 18:18:24 +0200

    Added ResponseEntity to CountryController and changed Lombok annotations of Country to @Data

--- src/main/java/com/javaSchool/FinalTask3/controllers/CountryController.java
+++ src/main/java/com/javaSchool/FinalTask3/controllers/CountryController.java
@@ -3,16 +3,21 @@ package com.javaSchool.FinalTask3.controllers;
 import com.javaSchool.FinalTask3.entity.Country;
 import com.javaSchool.FinalTask3.services.CountryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// TODO Specify HttpHeaders
+// TODO Add PutMapping
 @RequestMapping(path = "countries")
 @RequiredArgsConstructor
 @RestController
@@ -20,18 +25,28 @@ public class CountryController {
     private final CountryService service;
 
     @GetMapping
-    public List<Country> getAllCountries() {
-        return service.getAllCountries();
+    public ResponseEntity<List<Country>> getAllCountries() {
+        List<Country> countries = service.getAllCountries();
+
+        return new ResponseEntity<>(countries, HttpStatus.OK);
     }
 
     @GetMapping("/{name}")
-    public Country getCountryById(@PathVariable String name) {
-        return service.getCountryById(name);
+    public ResponseEntity<Country> getCountryById(@PathVariable String name) {
+        Country country = service.getCountryById(name);
+
+        return new ResponseEntity<>(country, HttpStatus.OK);
     }
 
     @PostMapping
-    public Country saveCountry(@RequestBody Country country) {
-        return service.saveCountry(country);
+    public ResponseEntity<Country> saveCountry(@RequestBody Country country) {
+        Country savedCountry = service.saveCountry(country);
+
+        if (savedCountry == null){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(service.saveCountry(country), HttpStatus.OK);
+        }
     }
 
     @DeleteMapping("/{name}")
--- src/main/java/com/javaSchool/FinalTask3/entity/Country.java
+++ src/main/java/com/javaSchool/FinalTask3/entity/Country.java
@@ -4,20 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.proxy.HibernateProxy;
 
-import java.util.Objects;
-
+@Data
 @Entity
-@Getter
 @RequiredArgsConstructor
-@Setter
 @Table(name = "countries", schema = "public", catalog = "online_store")
-@ToString
 public class Country {
     @Id
     @Column(name = "name", nullable = false)
@@ -25,20 +18,4 @@ public class Country {
 
     @Column(name = "is_active", nullable = false)
     private boolean isActive;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Country country = (Country) o;
-        return getName() != null && Objects.equals(getName(), country.getName());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
