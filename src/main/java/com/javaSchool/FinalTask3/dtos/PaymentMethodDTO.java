549080c9f98b43dc0487bdb3eebdc24e9adb511b
migup <migupele@gmail.com>
2023-10-06 13:54:09 +0200

    Implemented DTOs of DeliveryMethod, OrderStatus, PaymentMethod and PaymentStatus

---
+++ src/main/java/com/javaSchool/FinalTask3/dtos/PaymentMethodDTO.java
@@ -0,0 +1,8 @@
+package com.javaSchool.FinalTask3.dtos;
+
+import lombok.Data;
+
+@Data
+public class PaymentMethodDTO {
+    private String name;
+}
---
+++ src/main/java/com/javaSchool/FinalTask3/dtos/PaymentStatusDTO.java
@@ -0,0 +1,8 @@
+package com.javaSchool.FinalTask3.dtos;
+
+import lombok.Data;
+
+@Data
+public class PaymentStatusDTO {
+    private String name;
+}
