143f6e2029d60790803f572b19d3fc1f27319fdb
migup <migupele@gmail.com>
2023-10-09 19:21:18 +0200

    Refactoring Services (WIP)

--- src/main/java/com/javaSchool/FinalTask3/services/BookGenreService.java
+++ src/main/java/com/javaSchool/FinalTask3/services/BookGenreService.java
@@ -3,42 +3,40 @@ package com.javaSchool.FinalTask3.services;
 import com.javaSchool.FinalTask3.dtos.BookGenreDTO;
 import com.javaSchool.FinalTask3.entities.BookGenre;
 import com.javaSchool.FinalTask3.repositories.BookGenreRepository;
-import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RequiredArgsConstructor
+/**
+ * Service class responsible for the interaction between the {@link BookGenreRepository} and the
+ * {@link com.javaSchool.FinalTask3.controller.BookGenreController}. Obtains data from the {@link BookGenreRepository}
+ * and returns the object(s) of the entity {@link BookGenre} as {@link BookGenreDTO} to the
+ * {@link com.javaSchool.FinalTask3.controller.BookGenreController}.
+ */
 @Service
 @Transactional(readOnly = true)
-public class BookGenreService {
-    private final BookGenreRepository repository;
-
-    private final ModelMapper modelMapper;
-
-    public List<BookGenreDTO> getAllBookGenres(){
-        return repository.findAll()
-                .stream()
-                .map(bookGenre ->
-                        modelMapper.map(bookGenre, BookGenreDTO.class))
-                .collect(Collectors.toList());
+public class BookGenreService extends BaseService<BookGenre, BookGenreDTO, String>{
+    // TODO I can't figure out which annotations to use to make lombok work with the parent constructor. Tried with
+    //  @RequiredArgsConstructor in this class and @NoArgsConstructor(force = true) in parent class but then the
+    //  repository is null. I assumed that lombok automatically calls the corresponding super constructor of the parent
+    //  class, but I doesn't seem to be the case.
+    // TODO Or public BookGenreService(JpaRepository<BookGenre, String> repository, ModelMapper modelMapper) { and that
+    //  way I skip creating all the repositories?
+    /**
+     * All arguments constructor.
+     * @param repository {@link BookGenreRepository} of the {@link BookGenre} entity.
+     * @param modelMapper ModelMapper that converts the {@link BookGenre} to {@link BookGenreDTO}
+     */
+    public BookGenreService(BookGenreRepository repository, ModelMapper modelMapper) {
+        super(repository, modelMapper);
     }
 
-    public BookGenreDTO getBookGenreById(String name){
-        return modelMapper.map(repository.findById(name)
-                .orElse(null), BookGenreDTO.class);
-    }
-
-    @Transactional
-    public BookGenreDTO saveBookGenre(BookGenre bookGenre){
-        return modelMapper.map(repository.save(bookGenre), BookGenreDTO.class);
-    }
-
-    @Transactional
-    public void deleteBookGenre(String name){
-        repository.deleteById(name);
+    /**
+     * Returns the DTO class of the {@link BookGenre} entity.
+     * @return Returns the {@link BookGenreDTO} class.
+     */
+    @Override
+    protected Class<BookGenreDTO> getDTOClass() {
+        return BookGenreDTO.class;
     }
 }
--- src/main/java/com/javaSchool/FinalTask3/services/BookParametersFormatService.java
+++ src/main/java/com/javaSchool/FinalTask3/services/BookParametersFormatService.java
@@ -2,43 +2,36 @@ package com.javaSchool.FinalTask3.services;
 
 import com.javaSchool.FinalTask3.dtos.BookParametersFormatDTO;
 import com.javaSchool.FinalTask3.entities.BookParametersFormat;
-import com.javaSchool.FinalTask3.repositories.BookParameterFormatRepository;
-import lombok.RequiredArgsConstructor;
+import com.javaSchool.FinalTask3.repositories.BookGenreRepository;
+import com.javaSchool.FinalTask3.repositories.BookParametersFormatRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RequiredArgsConstructor
+/**
+ * Service class responsible for the interaction between the {@link BookParametersFormatRepository} and the
+ * {@link com.javaSchool.FinalTask3.controller.BookParametersFormatController}. Obtains data from the
+ * {@link BookParametersFormatRepository} and returns the object(s) of the entity {@link BookParametersFormat} as
+ * {@link BookParametersFormatDTO} to the {@link com.javaSchool.FinalTask3.controller.BookParametersFormatController}.
+ */
 @Service
 @Transactional(readOnly = true)
-public class BookParametersFormatService {
-    private final BookParameterFormatRepository repository;
-
-    private final ModelMapper modelMapper;
-
-    public List<BookParametersFormatDTO> getAllBookParametersFormat(){
-        return repository.findAll()
-                .stream()
-                .map(bookParameterFormat ->
-                        modelMapper.map(bookParameterFormat, BookParametersFormatDTO.class))
-                .collect(Collectors.toList());
+public class BookParametersFormatService extends BaseService<BookParametersFormat, BookParametersFormatDTO, String>{
+    /**
+     * All arguments constructor.
+     * @param repository {@link BookGenreRepository} of the {@link BookParametersFormat} entity.
+     * @param modelMapper ModelMapper that converts the {@link BookParametersFormat} to {@link BookParametersFormatDTO}
+     */
+    public BookParametersFormatService(BookParametersFormatRepository repository, ModelMapper modelMapper){
+        super(repository, modelMapper);
     }
 
-    public BookParametersFormatDTO getBookParametersFormatById(String name){
-        return modelMapper.map(repository.findById(name)
-                .orElse(null), BookParametersFormatDTO.class);
-    }
-
-    @Transactional
-    public BookParametersFormatDTO saveBookParametersFormat(BookParametersFormat bookParametersFormat){
-        return modelMapper.map(repository.save(bookParametersFormat), BookParametersFormatDTO.class);
-    }
-
-    @Transactional
-    public void deleteBookParametersFormat(String name){
-        repository.deleteById(name);
+    /**
+     * Returns the DTO class of the {@link BookParametersFormat} entity.
+     * @return Returns the {@link BookParametersFormatDTO} class.
+     */
+    @Override
+    protected Class<BookParametersFormatDTO> getDTOClass() {
+        return BookParametersFormatDTO.class;
     }
 }
