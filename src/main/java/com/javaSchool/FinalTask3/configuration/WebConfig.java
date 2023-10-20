619dffbb18e8143803c685ff7ba4e297c6bcf55f
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2023-10-20 13:21:28 +0200

    Implemented book genre manipulation

---
+++ src/main/java/com/javaSchool/FinalTask3/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package com.javaSchool.FinalTask3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer mvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "UPDATE", "DELETE").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
