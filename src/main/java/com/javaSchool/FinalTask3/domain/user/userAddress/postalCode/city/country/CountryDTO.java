61a8e66e48eb641b3a3b8f04c498f23f6a64f18b
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2023-11-14 15:59:43 +0100

    test: implemented test for country service

--- src/main/java/com/javaSchool/FinalTask3/domain/user/userAddress/postalCode/city/country/CountryDTO.java
+++ src/main/java/com/javaSchool/FinalTask3/domain/user/userAddress/postalCode/city/country/CountryDTO.java
@@ -1,5 +1,6 @@
 package com.javaSchool.FinalTask3.domain.user.userAddress.postalCode.city.country;
 
+import lombok.Builder;
 import lombok.Data;
 
 // TODO I am not sure if I should use @Data or @Value as some say DTO should be immutable and others say I should be
@@ -7,6 +8,7 @@ import lombok.Data;
 /**
  * Data Transfer Object (DTO) of {@link CountryEntity}.
  */
+@Builder
 @Data
 public class CountryDTO {
     private String name;
